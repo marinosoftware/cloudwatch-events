3ddc7819b2d4de2493425f2efe853737a85edc37
Dave O'Keeffe <dave@marinosoftware.com>
2018-05-17 16:17:56 +0100

    Swapped instance name and id

--- put-disk-space.sh
+++ put-disk-space.sh
@@ -6,8 +6,8 @@ used=`echo $root | awk -F " " '{print $3}'`
 free=`echo $root | awk -F " " '{print $4}'`
 percent=`echo $root | awk -F " " '{print $5}' | grep -o "[0-9]\+"`
 unit="Kilobytes"
-namespace="DS4"
-dimensions="Dimensions=[{Name=InstanceID,Value="$instance_id"},{Name=InstanceName,Value="\""$instance_name"\""}]"
+namespace="DS6"
+dimensions="Dimensions=[{Name=InstanceName,Value="\""$instance_name"\""},{Name=InstanceID,Value="$instance_id"}]"
 
 echo "DiskSpace:Total="$total",Used="$used",Free="$free",Percent="$percent
 
