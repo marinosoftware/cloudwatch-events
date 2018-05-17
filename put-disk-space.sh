f84ae2eb4e9c02c2ab647161ebd0ad08212df268
Dave O'Keeffe <dave@marinosoftware.com>
2018-05-17 08:54:09 +0100

    Added bash script for pushing disk-space metric

---
+++ put-disk-space.sh
@@ -0,0 +1,10 @@
+root=`df | grep "\W\/$"`
+total=`echo $root | awk -F " " '{print $2}'`
+used=`echo $root | awk -F " " '{print $3}'`
+free=`echo $root | awk -F " " '{print $4}'`
+percent=`echo $root | awk -F " " '{print $5}' | grep -o "[0-9]\+"`
+
+data="MetricName=DiskSpace,Dimensions=[{Name=Total,Value="$total"},{Name=Used,Value="$used"},{Name=Free,Value="$free"}],Unit=Kilobytes MetricName=DiskSpace,Dimensions=[{Name=Percent,Value="$percent"}],Unit=Percent"
+aws cloudwatch put-metric-data --namespace EC2 --metric-data "\""$data"\""
+echo "The disk has "$total" things, has used "$used" things, and has "$free" things free.  That's "$percent"%"
+
