0335a9845b6460e12168aa88874fe172b2d21032
Dave O'Keeffe <dave@marinosoftware.com>
2018-05-17 09:51:34 +0100

    Gave up on the Shorthand Syntax, went with single calls for each metric

--- put-disk-space.sh
+++ put-disk-space.sh
@@ -6,7 +6,10 @@ used=`echo $root | awk -F " " '{print $3}'`
 free=`echo $root | awk -F " " '{print $4}'`
 percent=`echo $root | awk -F " " '{print $5}' | grep -o "[0-9]\+"`
 
-data="MetricName=DiskSpace,Dimensions=[{Name=Total,Value="$total"},{Name=Used,Value="$used"},{Name=Free,Value="$free"}],Unit=Kilobytes MetricName=DiskSpace,Dimensions=[{Name=Percent,Value="$percent"}],Unit=Percent"
-aws cloudwatch put-metric-data --namespace EC2 --metric-data "\""$data"\""
+aws cloudwatch put-metric-data --namespace EC2 --metric-name DiskSpaceTotal --unit Kilobytes --value $total --dimensions InstanceID=$instance_id,InstanceType=$instance_type
+aws cloudwatch put-metric-data --namespace EC2 --metric-name DiskSpaceUsed --unit Kilobytes --value $used --dimensions InstanceID=$instance_id,InstanceType=$instance_type
+aws cloudwatch put-metric-data --namespace EC2 --metric-name DiskSpaceFree --unit Kilobytes --value $free --dimensions InstanceID=$instance_id,InstanceType=$instance_type
+aws cloudwatch put-metric-data --namespace EC2 --metric-name DiskSpacePercent --unit Percent --value $percent --dimensions InstanceID=$instance_id,InstanceType=$instance_type
+
 echo "The disk has "$total" things, has used "$used" things, and has "$free" things free.  That's "$percent"%"
 
