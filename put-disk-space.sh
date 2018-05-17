e16556fec730cbe7c526a07e0525530615926f9f
Dave O'Keeffe <dave@marinosoftware.com>
2018-05-17 10:12:16 +0100

    And back to kb...

--- put-disk-space.sh
+++ put-disk-space.sh
@@ -5,7 +5,7 @@ total=`echo $root | awk -F " " '{print $2}'`
 used=`echo $root | awk -F " " '{print $3}'`
 free=`echo $root | awk -F " " '{print $4}'`
 percent=`echo $root | awk -F " " '{print $5}' | grep -o "[0-9]\+"`
-unit="Bytes"
+unit="Kilobytes"
 
 aws cloudwatch put-metric-data --namespace EC2 --metric-name DiskSpaceTotal   --unit $unit    --value $total   --dimensions InstanceID=$instance_id,InstanceType=$instance_type
 aws cloudwatch put-metric-data --namespace EC2 --metric-name DiskSpaceUsed    --unit $unit    --value $used    --dimensions InstanceID=$instance_id,InstanceType=$instance_type
