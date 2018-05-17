8857207d22ec8f18ed71d039b5e47f8c03df80f4
Dave O'Keeffe <dave@marinosoftware.com>
2018-05-17 10:15:57 +0100

    Changed namespace, to give us better granularity on tests

--- put-disk-space.sh
+++ put-disk-space.sh
@@ -6,11 +6,12 @@ used=`echo $root | awk -F " " '{print $3}'`
 free=`echo $root | awk -F " " '{print $4}'`
 percent=`echo $root | awk -F " " '{print $5}' | grep -o "[0-9]\+"`
 unit="Kilobytes"
+namespace="DS1"
 
-aws cloudwatch put-metric-data --namespace EC2 --metric-name DiskSpaceTotal   --unit $unit    --value $total   --dimensions InstanceID=$instance_id,InstanceType=$instance_type
-aws cloudwatch put-metric-data --namespace EC2 --metric-name DiskSpaceUsed    --unit $unit    --value $used    --dimensions InstanceID=$instance_id,InstanceType=$instance_type
-aws cloudwatch put-metric-data --namespace EC2 --metric-name DiskSpaceFree    --unit $unit    --value $free    --dimensions InstanceID=$instance_id,InstanceType=$instance_type
-aws cloudwatch put-metric-data --namespace EC2 --metric-name DiskSpacePercent --unit Percent  --value $percent --dimensions InstanceID=$instance_id,InstanceType=$instance_type
+aws cloudwatch put-metric-data --namespace $namespace --metric-name DiskSpaceTotal   --unit $unit    --value $total   --dimensions InstanceID=$instance_id,InstanceType=$instance_type
+aws cloudwatch put-metric-data --namespace $namespace --metric-name DiskSpaceUsed    --unit $unit    --value $used    --dimensions InstanceID=$instance_id,InstanceType=$instance_type
+aws cloudwatch put-metric-data --namespace $namespace --metric-name DiskSpaceFree    --unit $unit    --value $free    --dimensions InstanceID=$instance_id,InstanceType=$instance_type
+aws cloudwatch put-metric-data --namespace $namespace --metric-name DiskSpacePercent --unit Percent  --value $percent --dimensions InstanceID=$instance_id,InstanceType=$instance_type
 
 echo "The disk has "$total" things, has used "$used" things, and has "$free" things free.  That's "$percent"%"
 
