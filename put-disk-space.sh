bd23a4d1595b5accfebeccd973c07a06f54576a1
Dave O'Keeffe <dave@marinosoftware.com>
2018-05-17 14:43:31 +0100

    Trying a refactor that uses the shorthand syntax again

--- put-disk-space.sh
+++ put-disk-space.sh
@@ -6,12 +6,19 @@ used=`echo $root | awk -F " " '{print $3}'`
 free=`echo $root | awk -F " " '{print $4}'`
 percent=`echo $root | awk -F " " '{print $5}' | grep -o "[0-9]\+"`
 unit="Kilobytes"
-namespace="DS2"
+namespace="DS3"
 
-aws cloudwatch put-metric-data --namespace $namespace --metric-name DiskSpaceTotal   --unit $unit    --value $total   --dimensions InstanceID=$instance_id,InstanceType=$instance_type
-aws cloudwatch put-metric-data --namespace $namespace --metric-name DiskSpaceUsed    --unit $unit    --value $used    --dimensions InstanceID=$instance_id,InstanceType=$instance_type
-aws cloudwatch put-metric-data --namespace $namespace --metric-name DiskSpaceFree    --unit $unit    --value $free    --dimensions InstanceID=$instance_id,InstanceType=$instance_type
-aws cloudwatch put-metric-data --namespace $namespace --metric-name DiskSpacePercent --unit Percent  --value $percent --dimensions InstanceID=$instance_id,InstanceType=$instance_type
+aws cloudwatch put-metric-data --metric-data \
+"MetricName=DiskSpaceTotal,Dimensions=[{InstanceID="$instance_id"}],Value="$total",Unit="$unit \
+"MetricName=DiskSpaceUsed,Dimensions=[{InstanceID="$instance_id"}],Value="$used",Unit="$unit \
+"MetricName=DiskSpaceFree,Dimensions=[{InstanceID="$instance_id"}],Value="$free",Unit="$unit \
+"MetricName=DiskSpacePercent,Dimensions=[{InstanceID="$instance_id"}],Value="$percent",Unit="$unit \
+--namespace $namespace
+
+# aws cloudwatch put-metric-data --namespace $namespace --metric-name DiskSpaceTotal   --unit $unit    --value $total   --dimensions InstanceID=$instance_id,InstanceType=$instance_type
+# aws cloudwatch put-metric-data --namespace $namespace --metric-name DiskSpaceUsed    --unit $unit    --value $used    --dimensions InstanceID=$instance_id,InstanceType=$instance_type
+# aws cloudwatch put-metric-data --namespace $namespace --metric-name DiskSpaceFree    --unit $unit    --value $free    --dimensions InstanceID=$instance_id,InstanceType=$instance_type
+# aws cloudwatch put-metric-data --namespace $namespace --metric-name DiskSpacePercent --unit Percent  --value $percent --dimensions InstanceID=$instance_id,InstanceType=$instance_type
 
 echo "The disk has "$total" things, has used "$used" things, and has "$free" things free.  That's "$percent"%"
 
