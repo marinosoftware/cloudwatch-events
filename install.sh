35f83e6719b1187c820fa46766fd2070b77a1d7f
Dave O'Keeffe <dave@marinosoftware.com>
2018-11-11 15:55:16 +0000

    Adds meminfo installation to the installation script

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 cp put-disk-space.cron /etc/cron.d/cloudwatch-put-disk-space
 chmod 644 /etc/cron.d/cloudwatch-put-disk-space
 chown root /etc/cron.d/cloudwatch-put-disk-space
+cp put-meminfo.cron /etc/cron.d/cloudwatch-put-meminfo
+chmod 644 /etc/cron.d/cloudwatch-put-meminfo
+chown root /etc/cron.d/cloudwatch-put-meminfo
 service crond restart
